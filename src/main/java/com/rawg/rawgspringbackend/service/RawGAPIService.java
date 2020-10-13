8e36e24fa46602c71d3553efb2f0405a698abe03
Szak0 <sattilabence@gmail.com>
2020-10-13 11:55:25 +0200

    Catch HttpClientErrorException

--- src/main/java/com/rawg/rawgspringbackend/service/RawGAPIService.java
+++ src/main/java/com/rawg/rawgspringbackend/service/RawGAPIService.java
@@ -2,9 +2,12 @@ package com.rawg.rawgspringbackend.service;
 
 
 import com.rawg.rawgspringbackend.model.generated.Games;
+import com.rawg.rawgspringbackend.model.generated.exception.InvalidEndpoint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -15,8 +18,13 @@ public class RawGAPIService {
 
     public Games getAllGames(String pageNumber, String search) {
         RestTemplate template = new RestTemplate();
-        ResponseEntity<Games> gamesResponseEntity = template.exchange("https://api.rawg.io/api/games?page=" + pageNumber,
-                HttpMethod.GET, null, Games.class);
-        return gamesResponseEntity.getBody();
+        try {
+            ResponseEntity<Games> gamesResponseEntity = template
+                    .exchange(gamesURL + "?page=" + pageNumber + "&search=" + search,
+                            HttpMethod.GET, null, Games.class);
+            return gamesResponseEntity.getBody();
+        } catch (HttpClientErrorException e) {
+            return new InvalidEndpoint("Not a valid endpoint");
+        }
     }
 }
