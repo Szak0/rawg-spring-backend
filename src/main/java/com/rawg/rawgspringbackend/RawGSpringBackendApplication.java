9080d1d77e77da66a708dc02b34a425bd6f39ed3
Szak0 <sattilabence@gmail.com>
2020-10-13 13:59:47 +0200

    Added app class

---
+++ src/main/java/com/rawg/rawgspringbackend/RawGSpringBackendApplication.java
@@ -0,0 +1,11 @@
+package com.rawg.rawgspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawGSpringBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RawGSpringBackendApplication.class, args);
+    }
+}
