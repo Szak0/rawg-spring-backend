dd7f155851f877370c19eae41cc2fa0ac9ae51f6
Szak0 <sattilabence@gmail.com>
2020-10-14 12:25:49 +0200

    Invalid endpoint object throw response status exception

--- src/main/java/com/rawg/rawgspringbackend/model/generated/exception/InvalidEndpoint.java
+++ src/main/java/com/rawg/rawgspringbackend/model/generated/exception/InvalidEndpoint.java
@@ -2,11 +2,14 @@ package com.rawg.rawgspringbackend.model.generated.exception;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.rawg.rawgspringbackend.model.generated.Games;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public class InvalidEndpoint extends Games {
 
 	public InvalidEndpoint(String detail) {
 		this.detail = detail;
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 	}
 
 	@JsonProperty("detail")
