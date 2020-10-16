68b144a1ba698f26b729821a385562274dbade90
Szak0 <sattilabence@gmail.com>
2020-10-16 15:02:19 +0200

    Test | GameById(id) service test

--- src/test/java/com/rawg/rawgspringbackend/RawGAPIServiceTest.java
+++ src/test/java/com/rawg/rawgspringbackend/RawGAPIServiceTest.java
@@ -1,28 +1,35 @@
 package com.rawg.rawgspringbackend;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import com.rawg.rawgspringbackend.model.generated.game.Game;
 import com.rawg.rawgspringbackend.service.RawGAPIService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.server.ResponseStatusException;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-
-@SpringBootTest
-@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(classes = {RawGAPIService.class})
+@RunWith(SpringRunner.class)
 public class RawGAPIServiceTest {
 
+    @Autowired
+    private RawGAPIService service;
+
     @Test
-    void testGameById() {
-        RestTemplate template = mock(RestTemplate.class);
-        RawGAPIService service = new RawGAPIService();
-        when();
-        service.getGameById("1");
+    public void testGameByIdReturnValueMatch() {
+        var mockId = "1";
+        Game game = service.getGameById(mockId);
+        assertEquals(Integer.parseInt(mockId), game.getId());
     }
 
+    @Test
+    public void testGameByIdMalformedId() {
+        var mockId = "1L";
+        assertThrows(ResponseStatusException.class, () -> {
+            service.getGameById(mockId);
+        });
+    }
 }
