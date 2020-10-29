eef0e66e15fd02fd94631b8ecf9927a4609c6bde
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-10-29 13:07:17 +0100

    WishlistController post wishlist

--- src/main/java/com/rawg/rawgspringbackend/controller/WishlistController.java
+++ src/main/java/com/rawg/rawgspringbackend/controller/WishlistController.java
@@ -3,10 +3,7 @@ package com.rawg.rawgspringbackend.controller;
 import com.rawg.rawgspringbackend.entity.WishlistItem;
 import com.rawg.rawgspringbackend.service.RawGAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +19,12 @@ public class WishlistController {
     public List<WishlistItem> gameList() {
         System.out.println(rawGAPIService.getWishlist());
         return rawGAPIService.getWishlist();
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping("/api/wishlist")
+    public void gameListSave(WishlistItem wishlistItem) {
+        
     }
 
 }
