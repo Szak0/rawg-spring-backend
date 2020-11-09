b15ee5638ad8b92f81d59416a579298245904c05
Szak0 <sattilabence@gmail.com>
2020-11-09 17:50:58 +0100

    Find user by email

--- src/main/java/com/rawg/rawgspringbackend/repository/UserRepository.java
+++ src/main/java/com/rawg/rawgspringbackend/repository/UserRepository.java
@@ -1,15 +1,8 @@
 package com.rawg.rawgspringbackend.repository;
-
 import com.rawg.rawgspringbackend.entity.RawGUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<RawGUser, Long> {
-
-
-    Optional<RawGUser> getRawGUserByEmailOrUserName(String email, String userName);
-
-
+    Optional<RawGUser> findByEmail(String email);
 }
