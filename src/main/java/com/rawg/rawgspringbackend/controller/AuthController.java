5e678674722d17c92a5ef769c54070e6d2de4c04
Szak0 <sattilabence@gmail.com>
2020-11-10 13:51:51 +0100

    Login and retrieve jwt model

--- src/main/java/com/rawg/rawgspringbackend/controller/AuthController.java
+++ src/main/java/com/rawg/rawgspringbackend/controller/AuthController.java
@@ -3,14 +3,25 @@ package com.rawg.rawgspringbackend.controller;
 import com.rawg.rawgspringbackend.model.UserCredentialsLogin;
 import com.rawg.rawgspringbackend.model.UserCredentialsRegister;
 import com.rawg.rawgspringbackend.model.error.ErrorInfo;
+import com.rawg.rawgspringbackend.security.JwtTokenServices;
 import com.rawg.rawgspringbackend.service.UserApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping
@@ -19,12 +30,13 @@ public class AuthController {
     @Autowired
     UserApiService userApiService;
 
-    @ExceptionHandler(value = {BadCredentialsException.class,
-            IllegalStateException.class, ResponseStatusException.class,
-            HttpClientErrorException.class})
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    protected ErrorInfo handleConflict(RuntimeException e) {
-        return new ErrorInfo(e);
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenServices jwtTokenServices) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenServices = jwtTokenServices;
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -37,9 +49,33 @@ public class AuthController {
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/auth/login")
     @ResponseBody
-    public String login(@RequestBody UserCredentialsLogin user) {
-        return "Logged in";
+    public ResponseEntity login(@RequestBody UserCredentialsLogin loginData) {
+        try {
+            String userEmail = loginData.getEmail();
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userEmail, loginData.getPassword()));
+            List<String> roles = authentication.getAuthorities()
+                    .stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+
+            String token = jwtTokenServices.createToken(userEmail, roles);
+
+            Map<Object, Object> model = new HashMap<>();
+            model.put("userEmail", userEmail);
+            model.put("roles", roles);
+            model.put("token", token);
+            return ResponseEntity.ok(model);
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid e-mail/password supplied");
+        }
     }
 
+    @ExceptionHandler(value = {BadCredentialsException.class,
+            IllegalStateException.class, ResponseStatusException.class,
+            HttpClientErrorException.class})
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    protected ErrorInfo handleConflict(RuntimeException e) {
+        return new ErrorInfo(e);
+    }
 
 }
