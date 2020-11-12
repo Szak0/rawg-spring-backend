cbd832ff5a6ee0a633e5fad42e17524e9a64b3c5
Szak0 <sattilabence@gmail.com>
2020-11-12 19:36:39 +0100

    Build 2 user for the demo

--- src/main/java/com/rawg/rawgspringbackend/RawGSpringBackendApplication.java
+++ src/main/java/com/rawg/rawgspringbackend/RawGSpringBackendApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -38,12 +39,21 @@ public class RawGSpringBackendApplication {
             RawGUser user = RawGUser
                     .builder()
                     .userName("Admin")
-                    .email("tomi1@meno.com")
+                    .email("admin@rawg.com")
                     .password(passwordEncoder.encode("tej123456"))
                     .registrationDate(LocalDateTime.now())
                     .roles(Arrays.asList("ROLE_USER", "ROLE_ADMIN"))
                     .build();
+            RawGUser user1 = RawGUser
+                    .builder()
+                    .userName("Gabor")
+                    .email("gabor@rawg.com")
+                    .password(passwordEncoder.encode("tej123456"))
+                    .registrationDate(LocalDateTime.now())
+                    .roles(Arrays.asList("ROLE_USER"))
+                    .build();
             userRepository.save(user);
+            userRepository.save(user1);
         };
     }
 
