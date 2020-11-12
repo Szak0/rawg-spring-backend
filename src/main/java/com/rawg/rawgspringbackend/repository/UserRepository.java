9a7e7f1eb23d928a2d73ea5811177ca1a654cb28
Szak0 <sattilabence@gmail.com>
2020-11-12 19:32:18 +0100

    Deleted | JPQL

--- src/main/java/com/rawg/rawgspringbackend/repository/UserRepository.java
+++ src/main/java/com/rawg/rawgspringbackend/repository/UserRepository.java
@@ -1,21 +1,9 @@
 package com.rawg.rawgspringbackend.repository;
 import com.rawg.rawgspringbackend.entity.RawGUser;
-import com.rawg.rawgspringbackend.entity.WishlistItem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import javax.transaction.Transactional;
 import java.util.Optional;
-import java.util.Set;
+
 
 public interface UserRepository extends JpaRepository<RawGUser, Long> {
     Optional<RawGUser> findByEmail(String email);
-    Optional<RawGUser> findByUserName(String username);
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE RawGUser raw SET raw.likedGames = ?1 where raw.email = ?2")
-    void updateUser(Set<WishlistItem> likedGamesSet, String userEmail );
-
 }
